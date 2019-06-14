538123c3b97f85f614dc1ea98379643e6ba3900a
parag <paraggarg66@yahoo.in>
2019-06-14 18:17:44 -0400

    Initial Commit

---
+++ day7reading/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day7reading
+//
+//  Created by Gurparkash Kaur on 2019-06-14.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
